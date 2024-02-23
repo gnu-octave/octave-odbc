6f199e3843e556f2019a5f22dea09c83556b5be4
JohnD <john.donoghue@ieee.org>
2024-02-23 12:18:04 -0500

    Add initial texinfo docs to .m files
    * INDEX: add listDataSources
    
    * inst/connection.m, inst/database.m,
      inst/listDataSources.m, inst/odbc.m: added texinfo toplevel docs

--- inst/database.m
+++ inst/database.m
@@ -1,4 +1,50 @@
-# create a database connection
+## Copyright (C) 2024 John Donoghue <john.donoghue@ieee.org>
+##
+## This program is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{conn} =} database (@var{dbname}, @var{username}, @var{password})
+## @deftypefnx {} {@var{conn} =} database (@var{dbname}, @var{username}, @var{password}, @var{propertyname}, @var{propertyvalue} @dots{})
+## Create a odbc database connection
+##
+## @subsubheading Inputs
+## @table @code
+## @item @var{dbname}
+## ODBC DSN connection name, or connection string
+## @item @var{username}
+## Username foe connecting to database.
+## @item @var{password}
+## Password for connecting to database.
+## @end table
+##
+## @subsubheading Outputs
+## @table @code
+## @item @var{conn}
+## A connection object for the connected database
+## @end table
+##
+## @subsubheading Examples
+## Open a a preconfigured default database, using blank username and password.
+## @example
+## @code {
+## db = database("default", "", "");
+## }
+## @end example
+##
+## @seealso{odbc, connection}
+## @end deftypefn
 
 function conn = database(databasename, username, password, varargin)
 
--- inst/listDataSources.m
+++ inst/listDataSources.m
@@ -1,4 +1,36 @@
-# list data sources
+## Copyright (C) 2024 John Donoghue <john.donoghue@ieee.org>
+##
+## This program is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{src} =} listDataSources ()
+## List available odbc datasources
+##
+## @subsubheading Inputs
+## None
+##
+## @subsubheading Outputs
+## @table @code
+## @item @var{src}
+## As table or cell structure of available data sources. The result contains fields for 
+## Name, DriverType and Vendor. 
+## @end table
+##
+## @seealso{odbc, database}
+## @end deftypefn
+
 function src = listDataSources ()
   src = __odbc_list__ ();
   if exist ("struct2dbtable") != 0
